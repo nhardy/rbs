5f366155486255eb3b66f8d267042264f9c5f70c
nhardy <nhardy@live.com.au>
2014-05-31 21:35:01 +1000

    Implemented login

--- db/user.py
+++ db/user.py
@@ -1,17 +1,58 @@
 from . import connection
+import hashlib, random, string
+
+def password_hash(password, salt):
+  return hashlib.sha256((password + salt).encode('utf-8')).hexdigest()
 
 class User:
-  def __init__(self, username, utype=1, uid=None):
+  @classmethod
+  def create(cls, username, utype, password):
+    cursor = connection.cursor()
+    row = cursor.execute('''
+      SELECT username FROM users WHERE username = ?
+    ''', (username,)).fetchone()
+    if row:
+      return False
+
+    salt = ''.join([random.choice(string.printable) for _ in range(32)])
+    password = password_hash(password, salt)
+    cursor.execute('''
+      INSERT INTO users (username, utype, password, salt)
+      VALUES (?, ?, ?, ?)
+    ''', (username, utype, password, salt))
+    connection.commit()
+    return cls(username, utype, cursor.lastrowid)
+
+  def __init__(self, username, utype, uid):
     self.username = username
     self.utype = utype
     self.uid = uid
 
     self._cursor = connection.cursor()
 
-    if self.uid is None:
-      self._cursor.execute('''
-        INSERT INTO users (username, utype)
-        VALUES (?, ?)
-      ''', (self.username, self.utype))
-      self.uid = self._cursor.lastrowid
-      connection.commit()
+  @classmethod
+  def login(cls, username, password):
+    cursor = connection.cursor()
+    row = cursor.execute('''
+      SELECT uid, utype, password, salt FROM users WHERE username = ?
+    ''', (username,)).fetchone()
+    if row is None:
+      return False
+
+    if row[2] == password_hash(password, row[3]):
+      return cls(username, row[1], row[0])
+    else:
+      return False
+
+  @classmethod
+  def from_username(cls, username):
+    cursor = connection.cursor()
+    cursor.execute('''
+      SELECT username, utype, uid FROM users WHERE username = ?
+    ''', (username,))
+    row = cursor.fetchone()
+
+    if row is None:
+      return None
+
+    return cls(row[0], row[1], row[2])
--- main.py
+++ main.py
@@ -2,16 +2,35 @@ import tornado.ioloop
 import tornado.web
 import tornado.template
 from objects.page import Page
+from db.user import User
+import random, string
+
+def current_user(handler):
+  username = handler.get_secure_cookie('username')
+  if username is None:
+    return None
+  return User.from_username(username.decode())
 
 template_loader = tornado.template.Loader('./templates')
 
 class MainHandler(tornado.web.RequestHandler):
   def get(self):
-    self.write(template_loader.load('content.html').generate(page=Page('Home',content='<p>Content</p>''')))
+    self.write(template_loader.load('content.html').generate(page=Page('Home',content='<p>Username:{}</p>'.format(current_user(self)))))
 
 class LoginHandler(tornado.web.RequestHandler):
-  def get(self):
-    self.write(template_loader.load('login.html').generate(page=Page('Login'), errors=[]))
+  def get(self, errors=False):
+    if current_user(self):
+      self.redirect('/')
+    self.write(template_loader.load('login.html').generate(page=Page('Login'), errors=errors))
+  def post(self):
+    entered_username = self.get_argument('username')
+    entered_password = self.get_argument('password')
+
+    user = User.login(entered_username, entered_password)
+    if not user:
+      self.get(['Incorrect user or password'])
+    self.set_secure_cookie('username', user.username)
+    self.redirect('/')
 
 handlers = [
   (r'/', MainHandler),
@@ -21,7 +40,7 @@ handlers = [
   (r'/fonts/(.*)',tornado.web.StaticFileHandler, {'path': './static/fonts/'})
 ]
 
-application = tornado.web.Application(handlers)
+application = tornado.web.Application(handlers, cookie_secret=''.join([random.choice(string.printable) for _ in range(63)]))
 
 if __name__ == '__main__':
   print('Server starting...')
