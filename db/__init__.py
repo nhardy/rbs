05e068ca3e68fa8b581eb5c24f71a56dd57c66bf
nhardy <nhardy@live.com.au>
2014-05-31 12:03:49 +1000

    Added automatic database creation
    Database connection variable: 'connection'

--- db/__init__.py
+++ db/__init__.py
@@ -1,1 +1,15 @@
-import sqlite3
+import os, sqlite3
+
+main_dir = '../' if __name__ == '__main__' else './'
+
+create_database = not os.path.exists('{}database.db'.format(main_dir))
+
+connection = sqlite3.connect('{}database.db'.format(main_dir))
+
+if create_database:
+  with open('{}db/createDatabase.sql'.format(main_dir)) as schema:
+    statements = schema.read().split(';')
+  cursor = connection.cursor()
+  for s in statements:
+    cursor.execute(s)
+  connection.commit()
