9b6558f274e6705a099be9909f7fe780d9ddbdfc
nhardy <nhardy@live.com.au>
2014-06-10 19:55:20 +1000

    Fix creation of database

--- rbs/db/__init__.py
+++ rbs/db/__init__.py
@@ -8,7 +8,7 @@ create_database = not os.path.exists('{}database.db'.format(main_dir))
 connection = sqlite3.connect('{}database.db'.format(main_dir))
 
 if create_database:
-  with open('{}db/createDatabase.sql'.format(main_dir)) as schema:
+  with open('{}rbs/db/createDatabase.sql'.format(main_dir)) as schema:
     statements = schema.read().split(';')
   cursor = connection.cursor()
   for s in statements:
