9b595e1edf7765ff6ec649958dcf3d21f3bdb141
nhardy <nhardy@live.com.au>
2014-06-11 14:03:18 +1000

    Created bookings handler

---
+++ rbs/handlers/bookings.py
@@ -0,0 +1,14 @@
+import tornado.web
+from .includes import template_loader, current_user
+from ..objects.page import Page
+from ..db.user import User
+from ..db.booking import Booking
+
+class BookingHandler(tornado.web.RequestHandler):
+  def page(self, user):
+    self.write(template_loader.load('bookings.html').generate(user=user, bookings=[], page=Page('Bookings')))
+  def get(self):
+    user = current_user(self)
+    if not user:
+      self.redirect('/')
+    self.page(user)
