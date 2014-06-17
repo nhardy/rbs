e12c43665e983e1c097524bb45aea0122903405f
nhardy <nhardy@live.com.au>
2014-06-17 23:05:12 +1000

    Add links

--- rbs/handlers/home.py
+++ rbs/handlers/home.py
@@ -7,5 +7,6 @@ class HomeHandler(tornado.web.RequestHandler):
     user = current_user(self)
     self.write(template_loader.load('content.html').generate(
       user=current_user(self),
-      page=Page('Home',content='<p>Welcome, ' + ('Guest' if user is None else user.username) +'!</p>')
+      page=Page('Home',content='<p>Welcome, ' + ('Guest' if user is None else user.username) + '!</p>\n' +
+      '''<p>This room booking system has been created by Nathan Hardy for the 2014 Software Design and Development Major Project. For instructional information, see the report or the <a href="https://github.com/nhardy/rbs/wiki">GitHub wiki</a></p>''')
     ))
